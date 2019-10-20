9f679faa8019991d8eec8fb5f3ee9703a3a111ca
Søren Lyhne <lyhnesoren@gmail.com>
2019-10-20 16:09:01 +0200

    install janitor

---
+++ theme/config/config.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+* This file contains definitions
+*
+* @package Config
+*/
+header("Content-type: text/html; charset=UTF-8");
+error_reporting(E_ALL);
+
+
+// Define current version
+define("VERSION", "0.7.8");
+
+
+/**
+* Site name
+*/
+define("SITE_UID", "LRKMSKNN");
+define("SITE_NAME", "lyrikmaskinen");
+define("SITE_URL", (isset($_SERVER["HTTPS"]) ? "https" : "http")."://".$_SERVER["SERVER_NAME"]);
+define("SITE_EMAIL", "lyhnesoren@gmail.com");
+
+/**
+* Optional constants
+*/
+define("DEFAULT_PAGE_DESCRIPTION", "");
+// define("DEFAULT_PAGE_IMAGE", "/img/logo-large.png");
+
+define("DEFAULT_LANGUAGE_ISO", "EN");
+define("DEFAULT_COUNTRY_ISO", "DK");
+// define("DEFAULT_CURRENCY_ISO", "DKK");
+
+
+// ENABLE ITEMS MODEL
+define("SITE_ITEMS", true);
+
+// define("SITE_SIGNUP", "/signup");
+// define("SITE_SUBSCRIPTIONS", true);
+// define("SITE_MEMBERS", true);
+
+// Enable shop model
+// define("SITE_SHOP", true);
+// define("SHOP_ORDER_NOTIFIES", "email@domain.tld");
+
+
+// Enable notifications (send collection email after N notifications)
+define("SITE_COLLECT_NOTIFICATIONS", 50);
+
+
+// INSTALL MODE (DISABLES ALL SECURITY) – ONLY USE IN EMERGENCIES AND ONLY TEMPORARILY
+// define("SITE_INSTALL", true);
+
+?>
---
+++ theme/config/segments.php
@@ -0,0 +1,27 @@
+<?php
+
+// local segmentation
+// setup default site runs only on desktop to minimize maintenance
+$segments_config["www"] = array(
+	
+	"desktop"       => "desktop",
+	"desktop_ie11"  => "desktop",
+	"desktop_ie10"  => "desktop",
+
+	"smartphone"    => "smartphone",
+
+	"desktop_ie9"   => "unsupported",
+	"desktop_light" => "unsupported",
+	"tv"            => "unsupported",
+
+	"tablet"        => "desktop",
+	"tablet_light"  => "desktop",
+
+	"mobile"        => "unsupported",
+	"mobile_light"  => "unsupported",
+
+	"seo"           => "seo"
+
+);
+
+?>
